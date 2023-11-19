7a49b06cfaf039369fc897d77c9a1bea5b56eaea
cololaborde <cololaborde@gmail.com>
2023-11-19 17:48:40 -0300

    create one table

---
+++ db/migrate/20231119193324_create_links.rb
@@ -0,0 +1,15 @@
+class CreateLinks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :links do |t|
+      t.string :slug, null: false, index: { unique: true }
+      t.string :destination_url, null: false
+      t.string :name, null: true
+      t.references :user, null: false, foreign_key: true
+      t.string :type, null: false
+      t.datetime :expiration_date
+      t.string :password
+      t.boolean :used
+      t.timestamps
+    end
+  end
+end
