eb5b683b09d5700e0113ba1ee3a939b0ed01729f
cololaborde <cololaborde@gmail.com>
2023-11-19 17:48:00 -0300

    remove validations migration

--- db/migrate/20231118164930_add_validations_to_regular_link.rb
+++
@@ -1,6 +0,0 @@
-class AddValidationsToRegularLink < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null :regular_links, :slug, false
-    change_column_null :regular_links, :destination_url, false
-  end
-end
--- db/migrate/20231118170517_add_validations_to_temporal_link.rb
+++
@@ -1,5 +0,0 @@
-class AddValidationsToTemporalLink < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null :temporal_links, :expiration_date, false
-  end
-end
