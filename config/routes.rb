ad943cdeb14ea5b5c05c1948a468ed5202a2ae9e
cololaborde <cololaborde@gmail.com>
2023-11-19 17:57:18 -0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :ephemeral_links
+  resources :private_links
+  resources :temporal_links
   root 'home#index'
   resources :regular_links
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
