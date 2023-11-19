249b5612e7d02b54b1849f818bd28cf3ae5d36cd
cololaborde <cololaborde@gmail.com>
2023-11-19 17:47:44 -0300

    several rollback

--- db/migrate/20231118170618_add_validations_to_private_link.rb
+++
@@ -1,5 +0,0 @@
-class AddValidationsToPrivateLink < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null :private_links, :password, false
-  end
-end
--- db/migrate/20231118170714_add_validations_to_ephemeral_link.rb
+++
@@ -1,5 +0,0 @@
-class AddValidationsToEphemeralLink < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null :ephemeral_links, :used, false
-  end
-end
