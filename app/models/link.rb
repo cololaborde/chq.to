a4dbf55c6004e7ce6f553869e2998dae5cc29431
cololaborde <cololaborde@gmail.com>
2023-11-21 20:56:01 -0300

    validate url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,8 +1,9 @@
 class Link < ApplicationRecord
-    belongs_to :user
-    validates_presence_of :slug, :destination_url
-    validates_uniqueness_of :slug
-    validates :name, presence: false
-    validates :type, presence: true
+  belongs_to :user
+  validates_presence_of :slug, :destination_url
+  validates_uniqueness_of :slug
+  validates :name, presence: false
+  validates :type, presence: true
+  validates :destination_url, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
   end
   
