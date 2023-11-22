a52a4c850d8c5a6c03101e7d0215415d8a0a339a
cololaborde <cololaborde@gmail.com>
2023-11-22 20:35:48 -0300

    fix expiration date validation

--- app/models/temporal_link.rb
+++ app/models/temporal_link.rb
@@ -4,8 +4,8 @@ class TemporalLink < Link
   validate :expiration_date_cannot_be_in_the_past
 
   def expiration_date_cannot_be_in_the_past
-    if expiration_date.present? && expiration_date < DateTime.now
-      errors.add("La fecha de vencimiento debe ser posterior a la actual.")
+    if expiration_date.present? && expiration_date <= DateTime.current
+      errors.add(:expiration_date, "La fecha y hora de vencimiento deben ser posteriores a la actual.")
     end
   end
 end
