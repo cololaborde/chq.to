cb5b7d751d5bdb85131ea612d76e4cd1475f9125
cololaborde <cololaborde@gmail.com>
2023-11-25 17:20:02 -0300

    add link access filter

--- app/controllers/link_accesses_controller.rb
+++ app/controllers/link_accesses_controller.rb
@@ -1,9 +1,11 @@
 class LinkAccessesController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_link_accesses, only: %i[index]
 
     def index
-        @link_accesses = current_user.links.where(id: params[:link_id]).includes(:link_accesses).flat_map(&:link_accesses)
-    end      
+        @q = @accesses.ransack(params[:q])
+        @link_accesses = @q.result()
+      end     
     
     def show
         @link_access = LinkAccess.find(params[:id])
@@ -24,4 +26,18 @@ class LinkAccessesController < ApplicationController
     def link_access_params
         params.require(:link_access).permit(:link_id, :accessed_at, :ip_address)
     end
+
+    def set_link_accesses
+        @q = LinkAccess.ransack(params[:q])
+      
+        if params[:link_id].present?
+          # Se proporcionó link_id, filtramos por ese enlace específico
+          @link = current_user.links.find(params[:link_id])
+          @accesses = @link.link_accesses.merge(@q.result)
+        else
+          # No se proporcionó link_id, obtenemos resultados según los parámetros de Ransack
+          @accesses = @q.result(distinct: true)
+        end
+      end
+      
 end
--- app/models/link_access.rb
+++ app/models/link_access.rb
@@ -1,3 +1,8 @@
 class LinkAccess < ApplicationRecord
   belongs_to :link
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[accessed_at ip_address]
+  end
+
 end
