1c9ed2cab1654bd823c7ddc3e623edf1fbae0b04
cololaborde <cololaborde@gmail.com>
2023-11-26 15:33:09 -0300

    remove unused && add access per day

--- app/controllers/link_accesses_controller.rb
+++ app/controllers/link_accesses_controller.rb
@@ -3,23 +3,10 @@ class LinkAccessesController < ApplicationController
     before_action :set_link_accesses, only: %i[index]
 
     def index
-        @q = @accesses.ransack(params[:q])
-        @link_accesses = @q.result()
-      end     
-    
-    def show
-        @link_access = LinkAccess.find(params[:id])
-    end
-
-    def create
-        @link_access = LinkAccess.new(link_access_params)
-    
-        if @link_access.save
-          render json: { status: 'success', message: 'Link access created successfully' }
-        else
-          render json: { status: 'error', message: 'Failed to create link access', errors: @link_access.errors.full_messages }
-        end
-    end
+      @q = @accesses.ransack(params[:q])
+      @link_accesses = @q.result()
+      set_link_access_per_day
+    end     
     
     private
     
@@ -35,9 +22,11 @@ class LinkAccessesController < ApplicationController
           @link = current_user.links.find(params[:link_id])
           @accesses = @link.link_accesses.merge(@q.result)
         else
-          # No se proporcionó link_id, obtenemos resultados según los parámetros de Ransack
-          @accesses = @q.result(distinct: true)
+          render file: "#{Rails.root}/public/404.html", status: :not_found, layout: false
         end
       end
-      
+
+    def set_link_access_per_day
+      @access_per_day = LinkAccess.where(link_id: params[:link_id]).group("strftime('%Y-%m-%d', accessed_at)").count
+    end
 end
