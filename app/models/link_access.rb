48d167f025208697f68d9b8c1bc7cc777d64fda3
cololaborde <cololaborde@gmail.com>
2023-11-27 18:59:54 -0300

    link access validations

--- app/models/link_access.rb
+++ app/models/link_access.rb
@@ -1,5 +1,7 @@
 class LinkAccess < ApplicationRecord
   belongs_to :link
+  validates :accessed_at, presence: true
+  validates :ip_address, presence: true
 
   # Setea los atributos para permitir la busqueda con ransack
   def self.ransackable_attributes(auth_object = nil)
