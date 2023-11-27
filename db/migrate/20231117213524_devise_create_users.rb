657b861671d0e83db1673e21c082a1b21a6c0d34
cololaborde <cololaborde@gmail.com>
2023-11-27 20:57:43 -0300

    prueba

--- db/migrate/20231117213524_devise_create_users.rb
+++ db/migrate/20231117213524_devise_create_users.rb
@@ -7,13 +7,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-
-      ## Rememberable
-      t.datetime :remember_created_at
-
       t.string :user_name, null: false, default: ""
       t.string :name, null: false, default: ""
 
@@ -21,7 +14,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
     add_index :users, :user_name,            unique: true
   end
 end
