3ca7dc9873c87ec2bbbf8a02e95456b87b895bc1
cololaborde <cololaborde@gmail.com>
2023-11-27 19:00:22 -0300

    clean && remove unused

--- db/migrate/20231119193324_create_links.rb
+++ db/migrate/20231119193324_create_links.rb
@@ -3,7 +3,7 @@ class CreateLinks < ActiveRecord::Migration[7.1]
     create_table :links do |t|
       t.string :slug, null: false, index: { unique: true }
       t.string :destination_url, null: false
-      t.string :name, null: true
+      t.string :name
       t.references :user, null: false, foreign_key: true
       t.string :type, null: false
       t.datetime :expiration_date
