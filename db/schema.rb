7e4849cf16f3cc0c26de534541ad48b4c7b410b3
cololaborde <cololaborde@gmail.com>
2023-11-27 21:02:54 -0300

    prueba 2

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_224744) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
     t.string "user_name", default: "", null: false
     t.string "name", default: "", null: false
     t.datetime "created_at", null: false
