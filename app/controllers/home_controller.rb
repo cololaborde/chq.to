df5aa77459feec6890f137351a4813af4e33b66f
cololaborde <cololaborde@gmail.com>
2023-11-28 18:48:36 -0300

    show top 5 accessed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,20 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @regular_links = RegularLink.all.where({ user_id: current_user.id})
-    @temporal_links = TemporalLink.all.where(user_id: current_user.id)
-    @private_links = PrivateLink.all.where(user_id: current_user.id)
-    @ephemeral_links = EphemeralLink.all.where(user_id: current_user.id)
+    @regular_links = get_top_links(RegularLink)
+    @temporal_links = get_top_links(TemporalLink)
+    @private_links = get_top_links(PrivateLink)
+    @ephemeral_links = get_top_links(EphemeralLink)
+  end
+
+  private
+
+  def get_top_links(link_type)
+    link_type
+      .left_outer_joins(:link_accesses)
+      .where(user_id: current_user.id)
+      .group('links.id')
+      .order('COUNT(link_accesses.id) DESC')
+      .limit(5)
   end
 end
